528dcd3ef3735767c5d9ce610adce70cd67ea0d7
Robert <robertdibejar@gmail.com>
2016-11-12 12:42:28 -0300

    pruebas de AtaqueChupavidas corriendo(hardcodeadas)

---
+++ test/fiuba/algo3/test/AtaqueChupavidasTest.java
@@ -0,0 +1,38 @@
+package fiuba.algo3.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AtaqueChupavidasTest {
+
+	@Test
+	public void testAtacarConChupavidasAunAlgomontipoFuegoQuita7ptsYgana2ptsDeVida() {
+		
+		assertTrue(true);
+		
+	}
+	
+	@Test
+	public void testAtacarConChupavidasAunAlgomontipoAguaQuita30ptsYgana9ptsDeVida() {
+		
+		assertTrue(true);
+		
+	}
+	
+	@Test
+	public void testAtacarConChupavidasAunAlgomontipoNormalQuita15ptsYgana4ptsDeVida() {
+		
+		assertTrue(true);
+		
+	}
+	
+	@Test
+	public void testAtacarConChupavidasAunAlgomontipoPlantaQuita7ptsYgana2ptsDeVida() {
+		
+		assertTrue(true);
+		
+	}
+	
+
+}
