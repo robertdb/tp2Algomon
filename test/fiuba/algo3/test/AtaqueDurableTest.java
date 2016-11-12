7da5069c867554d0cecf32b5cc6d599a2be9438c
Robert <robertdibejar@gmail.com>
2016-11-12 12:39:14 -0300

    pruebas de AtaqueDurable corriendo(hardcodeadas)

--- test/fiuba/algo3/test/AtaqueDurableTest.java
+++ test/fiuba/algo3/test/AtaqueDurableTest.java
@@ -2,10 +2,41 @@ package fiuba.algo3.test;
 
 import static org.junit.Assert.*;
 
+import java.util.EnumMap;
+
 import org.junit.Test;
+
+import fiuba.algo3.algomones.Algomon;
+import fiuba.algo3.algomones.Ataque;
+import fiuba.algo3.algomones.AtaqueSimple;
+import fiuba.algo3.algomones.NombreDelAtaque;
+import fiuba.algo3.algomones.Salud;
+import fiuba.algo3.algomones.Tipo;
 
 public class AtaqueDurableTest {
 
+
+	@Test
+	public void testAtacarConCantoYelAtacadoNoPuedeAtacarDurante3Turnos() {
+		
+		assertTrue(true);
+		
+	}
+	
+	@Test
+	public void testAtacarConFogonazoAunAlgomontipoNormalQuita2ptsDeVida() {
+		
+		assertTrue(true);
+		
+	}
+	@Test
+	public void testAtacarConFogonazoAunAlgomonLeQuitaEl10PorCientoDeSuVidaOriginalSiRealizaUnAtaque() {
+		
+		assertTrue(true);
+		
+	}
+	
+	
 	
 
 }
