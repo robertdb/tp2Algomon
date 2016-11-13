666b51bbe29c83289880100399b7beec9fcc5d9f
Robert <robertdibejar@gmail.com>
2016-11-13 13:53:51 -0300

    AtaqueCantoTest y AtaqueFogonazoTest creadas con tests hardcodeados

---
+++ test/fiuba/algo3/test/AtaqueCantoTest.java
@@ -0,0 +1,15 @@
+package fiuba.algo3.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AtaqueCantoTest {
+
+	@Test
+	public void testAtacarConCantoYelAtacadoNoPuedeAtacarDurante3Turnos() {
+		
+		assertTrue(true);
+		
+	}
+}
---
+++ test/fiuba/algo3/test/AtaqueFogonazoTest.java
@@ -0,0 +1,22 @@
+package fiuba.algo3.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AtaqueFogonazoTest {
+
+	@Test
+	public void testAtacarConFogonazoAunAlgomontipoNormalQuita2ptsDeVida() {
+		
+		assertTrue(true);
+		
+	}
+	@Test
+	public void testAtacarConFogonazoAunAlgomonLeQuitaEl10PorCientoDeSuVidaOriginalSiRealizaUnAtaque() {
+		
+		assertTrue(true);
+		
+	}
+
+}
