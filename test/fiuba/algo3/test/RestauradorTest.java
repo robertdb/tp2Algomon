f59900ae45256d5129b33ab7c40dcdbdc50d6750
Robert <robertdibejar@gmail.com>
2016-11-15 12:05:08 -0300

    testAlgomonAgotoLasPosionesYnoPuedeUtilizarMasPosiones()

--- test/fiuba/algo3/test/RestauradorTest.java
+++ test/fiuba/algo3/test/RestauradorTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import fiuba.algo3.algomones.excepciones.RestauradorAgotadoException;
+import fiuba.algo3.algomones.excepciones.SuperPosionAgotadaException;
+
 public class RestauradorTest {
 
 	@Test
@@ -14,6 +17,12 @@ public class RestauradorTest {
 	@Test
 	public void testUsarRestauradorEnAlgomonBajoEfectoDomidoVuelveAestadoNormal() {
 		assertTrue(true);
+	}
+	
+	@Test(expected = RestauradorAgotadoException.class)
+	public void testAlgomonAgotoLasPosionesYnoPuedeUtilizarMasPosiones(){
+		
+		throw new RestauradorAgotadoException();
 	}
 	
 }
