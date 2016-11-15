008a19c5f29b4b6af7ac0dbd940ac897170c5ddf
Robert <robertdibejar@gmail.com>
2016-11-15 11:49:32 -0300

    nueva clase RestauradorTest y testUsarRestauradorEnAlgomonBajoEfectoQuemadoVuelveAestadoNormal()

---
+++ test/fiuba/algo3/test/RestauradorTest.java
@@ -0,0 +1,13 @@
+package fiuba.algo3.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RestauradorTest {
+
+	@Test
+	public void testUsarRestauradorEnAlgomonBajoEfectoQuemadoVuelveAestadoNormal() {
+		assertTrue(true);
+	}
+}
