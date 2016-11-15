b272383552ac244bf6597ac6d2c4ee0787de204c
Robert <robertdibejar@gmail.com>
2016-11-15 11:11:17 -0300

    testUsarPosionEnAlgomonCon20ptsDeDanioRecuperaTodaSuVida()

---
+++ test/fiuba/algo3/test/PosionTest.java
@@ -0,0 +1,14 @@
+package fiuba.algo3.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PosionTest {
+
+	@Test
+	public void testUsarPosionEnAlgomonCon20ptsDeDanioRecuperaTodaSuVida() {
+		assertTrue(true);
+	}
+
+}
