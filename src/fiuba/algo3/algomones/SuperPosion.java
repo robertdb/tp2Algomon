d7a82a7824d531aecc6dde07f0f9213202fdd287
Robert <robertdibejar@gmail.com>
2016-11-15 15:25:27 -0300

    SuperPosionTest corriendo sin harcodeo

--- src/fiuba/algo3/algomones/SuperPosion.java
+++ src/fiuba/algo3/algomones/SuperPosion.java
@@ -1,10 +1,41 @@
 package fiuba.algo3.algomones;
 
+import fiuba.algo3.algomones.excepciones.PosionAgotadaException;
+import fiuba.algo3.algomones.excepciones.SuperPosionAgotadaException;
+
 public class SuperPosion implements Elemento {
 
+	Elemento posionSimple1;
+	Elemento posionSimple2;
+	
+	public SuperPosion(){
+		
+		this.posionSimple1 = new Posion();
+		
+		this.posionSimple2 = new Posion();
+		
+	}
+	
 	@Override
 	public void aplicarElemento(Algomon algomon) {
-		// TODO Auto-generated method stub
+		
+		try{
+			this.posionSimple1.aplicarElemento(algomon);
+			this.posionSimple1.aplicarElemento(algomon);
+		}
+		catch(PosionAgotadaException e){
+			
+			try{
+				this.posionSimple2.aplicarElemento(algomon);
+				this.posionSimple1.aplicarElemento(algomon);
+			}
+			catch(PosionAgotadaException _e){
+				
+				throw new SuperPosionAgotadaException();
+				
+			}
+			
+		}
 
 	}
 
--- test/fiuba/algo3/test/SuperPosionTest.java
+++ test/fiuba/algo3/test/SuperPosionTest.java
@@ -14,6 +14,7 @@ import fiuba.algo3.algomones.NombreDelAtaque;
 import fiuba.algo3.algomones.Posion;
 import fiuba.algo3.algomones.Salud;
 import fiuba.algo3.algomones.SuperPosion;
+import fiuba.algo3.algomones.TipoAgua;
 import fiuba.algo3.algomones.TipoNormal;
 import fiuba.algo3.algomones.excepciones.PosionAgotadaException;
 import fiuba.algo3.algomones.excepciones.SuperPosionAgotadaException;
@@ -22,8 +23,41 @@ public class SuperPosionTest {
 
 	@Test
 	public void testUsarSuperPosionEnAlgomonCon40ptsDeDanioRecuperaTodaSuVida() {
-		assertTrue(true);
-	}
+		
+		// Se crea un algomon personalizado.
+		int potenciaCanionDeAgua = 20;
+		int cantidadMaximaDeAtaques = 8;
+		Ataque canionDeAgua = new AtaqueSimple(new TipoAgua(), potenciaCanionDeAgua , cantidadMaximaDeAtaques);
+		EnumMap<NombreDelAtaque, Ataque> _ataques = new EnumMap<NombreDelAtaque, Ataque >(NombreDelAtaque.class);
+		_ataques.put(NombreDelAtaque.CANION_DE_AGUA, canionDeAgua);
+		Salud saludBlastoise = new Salud(400);
+		Algomon blastoise = new Algomon("Blastoise", new TipoAgua(), _ataques , saludBlastoise);
+						
+				
+		// Se crea otro algomon personalizado.
+		int potenciaAtaqueRapido = 25;
+		int cantidadMaximaDeAtaquesAtaqueRapido = 16;
+		Ataque ataqueRapido = new AtaqueSimple(new TipoNormal(), potenciaAtaqueRapido , cantidadMaximaDeAtaquesAtaqueRapido );
+		EnumMap<NombreDelAtaque, Ataque> ataquesNormal = new EnumMap<NombreDelAtaque, Ataque >(NombreDelAtaque.class);
+		ataquesNormal.put(NombreDelAtaque.ATAQUE_RAPIDO, ataqueRapido);
+		Salud saludRaticate = new Salud(300);
+		Algomon raticate = new Algomon("Raticate", new TipoNormal(), ataquesNormal, saludRaticate);
+				
+		int vidaOriginalRaticate  = raticate.salud();
+				
+		blastoise.atacar(raticate, NombreDelAtaque.CANION_DE_AGUA);
+		blastoise.atacar(raticate, NombreDelAtaque.CANION_DE_AGUA);
+				
+		assertEquals(vidaOriginalRaticate - 40,  raticate.salud() );
+				
+		Elemento superPosion = new SuperPosion();
+				
+		superPosion.aplicarElemento(raticate);
+				
+		assertEquals(vidaOriginalRaticate , raticate.salud() );
+				
+	}	
+	
 	
 	@Test
 	public void testUsarSuperPosionEnAlgomonConVidaSinSerDaniadaNoRecuperaPtsDeVida() {
@@ -46,7 +80,8 @@ public class SuperPosionTest {
 	
 		superPosion.aplicarElemento(raticate);
 		superPosion.aplicarElemento(raticate);
-		throw new SuperPosionAgotadaException();
+		superPosion.aplicarElemento(raticate);
+		
 	}
 	
 }
