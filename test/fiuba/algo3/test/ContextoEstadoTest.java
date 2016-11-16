51d450f5b05a694301b619fd9151164f02cce6f8
OrlandoEspinoza <ole22_dbz@hotmail.com>
2016-11-15 22:15:36 -0300

    test inicial contexto

--- test/fiuba/algo3/test/ContextoEstadoTest.java
+++ test/fiuba/algo3/test/ContextoEstadoTest.java
@@ -4,11 +4,34 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import fiuba.algo3.algomones.AlgomonEstado;
+import fiuba.algo3.algomones.ContextoEstado;
+import fiuba.algo3.algomones.Dormido;
+import fiuba.algo3.algomones.Efecto;
+import fiuba.algo3.algomones.EstadoEfimero;
+import fiuba.algo3.algomones.EstadoPersistente;
+import fiuba.algo3.algomones.Quemado;
+
 public class ContextoEstadoTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testContextoEstadoInicial() {
+		ContextoEstado contexto = new ContextoEstado(); 
+		
+		assertEquals(false,contexto.estaAfectado());
+		
+		Efecto dormido = new Dormido();
+		AlgomonEstado efimero = new EstadoEfimero(dormido);
+		contexto.estadoNuevo(efimero);
+		
+		Efecto quemado = new Quemado();
+		AlgomonEstado persistente = new EstadoPersistente(quemado);
+		contexto.estadoNuevo(persistente);
+		
+		assertEquals(true,contexto.estaAfectado());
+		
+		
+		
 	}
 
 }
