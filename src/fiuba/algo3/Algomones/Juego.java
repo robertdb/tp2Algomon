d21f2d9e05939e77c71b8793e801dee3ff0cc7a2
OrlandoEspinoza <ole22_dbz@hotmail.com>
2016-11-22 23:09:37 -0300

    clase juego creado

---
+++ src/fiuba/algo3/Algomones/Juego.java
@@ -0,0 +1,31 @@
+package fiuba.algo3.Algomones;
+
+import fiuba.algo3.algomones.Jugador;
+
+public class Juego {
+	private Jugador activo;
+	private Jugador pasivo;
+
+	public Juego(Jugador jugador1, Jugador jugador2) {
+		activo = jugador1;
+		pasivo = jugador2;
+	}
+
+	public Jugador jugadorActivo() {
+		// TODO Auto-generated method stub
+		return activo;
+	}
+
+	public void siguienteTruno() {
+		Jugador aux = activo;
+		activo = pasivo;
+		pasivo = aux;
+		
+	}
+
+	public Jugador jugadorPasivo() {
+		// TODO Auto-generated method stub
+		return pasivo;
+	}
+
+}
---
+++ test/fiuba/algo3/test/juegoTest.java
@@ -0,0 +1,30 @@
+package fiuba.algo3.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fiuba.algo3.Algomones.Juego;
+import fiuba.algo3.algomones.Jugador;
+
+public class juegoTest {
+
+	@Test
+	public void test() {
+		Jugador jugador1 = new Jugador(null,null,null);
+		Jugador jugador2 = new Jugador(null,null,null);
+		
+		Juego juego = new Juego(jugador1,jugador2);
+		
+		Jugador jugadorAux = juego.jugadorActivo();
+		assertTrue(jugadorAux == jugador1);
+		
+		juego.siguienteTruno();
+		
+		jugadorAux = juego.jugadorActivo();
+		assertFalse(jugadorAux == jugador1);
+		
+		assertTrue(jugador1 == juego.jugadorPasivo());
+	}
+
+}
