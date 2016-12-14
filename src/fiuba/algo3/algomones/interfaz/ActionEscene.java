d4b814c135f07269b331a1fdcbe65dc1b93fb961
OrlandoEspinoza <ole22_dbz@hotmail.com>
2016-12-14 11:07:46 -0300

    prueba de sonido

--- src/fiuba/algo3/algomones/interfaz/ActionEscene.java
+++ src/fiuba/algo3/algomones/interfaz/ActionEscene.java
@@ -1,5 +1,6 @@
 package fiuba.algo3.algomones.interfaz;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
@@ -8,6 +9,9 @@ import fiuba.algo3.algomones.Juego;
 import fiuba.algo3.algomones.NombreDelAtaque;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
 public class ActionEscene {
@@ -31,7 +35,14 @@ public class ActionEscene {
 	    this.ataques.actualizarBotonesAtaque(juego.getActivo().getAtaqueAlgomon(),juego.getActivo(),juego.getPasivo());
 	    this.elementos.actualizarBotonesElementos(juego.getActivo().getElementos(),juego.getActivo());
 	    this.algomones.actualizarBotonesAlgomones(juego.getActivo().getAlgomones(),juego.getActivo());
-		
+	    File file = new File("music/"+"Tinitus.mp3".toLowerCase());
+	    file.getAbsolutePath();
+	    Media media = new Media(file.toURI().toString());
+	    MediaPlayer mediaPlayer = new MediaPlayer(media);
+	    mediaPlayer.setAutoPlay(true);
+	    MediaView mediaView = new MediaView(mediaPlayer);
+	    
+	    root.getChildren().add(mediaView);
 	}
 
 	public void actualizarBotones() {
