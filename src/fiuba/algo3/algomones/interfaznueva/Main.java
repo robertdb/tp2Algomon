a47e102ac9b7969b9a8cdc10e9c13050c252e4fb
Nicolas Giraldez <klaus.15@gmail.com>
2016-12-15 15:40:36 -0300

    Musica de fondo en interfaznueva

--- src/fiuba/algo3/algomones/interfaznueva/Main.java
+++ src/fiuba/algo3/algomones/interfaznueva/Main.java
@@ -8,10 +8,11 @@ import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 	
 	private Juego juego;
+	
+	private MusicaDeFondo musica = new MusicaDeFondo();
 	
 	public static void main(String[] args) {
         launch(args);
@@ -45,6 +46,8 @@ public class Main extends Application {
     	stage.show();
     	
     	controller.setVentanaActual(stage);
+    	
+    	musica.run();
     	
     }
     
